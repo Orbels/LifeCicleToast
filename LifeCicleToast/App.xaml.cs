c204091ea6ca4d47813a33e7ae2535dffdb5ed1d
Martin <Martin@MARTINPC>
2016-11-22 11:38:05 +0100

    Agregar archivos de proyecto.

---
+++ LifeCicleToast/App.xaml.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using NotificationsExtensions;
+using System.Threading.Tasks;
+using NotificationsExtensions.Toasts;
+using Windows.UI.Notifications;
+
+namespace LifeCicleToast
+{
+    /// <summary>
+    /// Proporciona un comportamiento específico de la aplicación para complementar la clase Application predeterminada.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// Inicializa el objeto de aplicación Singleton. Esta es la primera línea de código creado
+        /// ejecutado y, como tal, es el equivalente lógico de main() o WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+            this.EnteredBackground += App_EnteredBackground;
+            this.LeavingBackground += App_LeavingBackground;
+        }
+
+        private void App_LeavingBackground(object sender, LeavingBackgroundEventArgs e)
+        {
+            NotificarLifeCicle("Sale del segundo plano");
+        }
+
+        private void App_EnteredBackground(object sender, EnteredBackgroundEventArgs e)
+        {
+            NotificarLifeCicle("Entra en segundo plano");
+        }
+
+        /// <summary>
+        /// Se invoca cuando el usuario final inicia la aplicación normalmente. Se usarán otros puntos
+        /// de entrada cuando la aplicación se inicie para abrir un archivo específico, por ejemplo.
+        /// </summary>
+        /// <param name="e">Información detallada acerca de la solicitud y el proceso de inicio.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            //#if DEBUG
+            //            if (System.Diagnostics.Debugger.IsAttached)
+            //            {
+            //                this.DebugSettings.EnableFrameRateCounter = true;
+            //            }
+            //#endif
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // No repetir la inicialización de la aplicación si la ventana tiene contenido todavía,
+            // solo asegurarse de que la ventana está activa.
+            if (rootFrame == null)
+            {
+                // Crear un marco para que actúe como contexto de navegación y navegar a la primera página.
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Cargar el estado de la aplicación suspendida previamente
+                }
+
+                // Poner el marco en la ventana actual.
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                NotificarLifeCicle("El susario ha imiciado la aplicación");
+
+                if (rootFrame.Content == null)
+                {
+                    // Cuando no se restaura la pila de navegación, navegar a la primera página,
+                    // configurando la nueva página pasándole la información requerida como
+                    //parámetro de navegación
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                // Asegurarse de que la ventana actual está activa.
+                Window.Current.Activate();
+            }
+            else if (e.PrelaunchActivated == true)
+            {
+                NotificarLifeCicle("Relanzado");
+            }
+            string estadoAnterior = e.PreviousExecutionState.ToString();
+            NotificarLifeCicle(estadoAnterior);
+        }
+
+        private static void NotificarLifeCicle(string texto)
+        {
+            //TODO: crear la Notificacion y lanzarla
+            //Notificacion de inicio de la App por el usuario
+            //--> PrelaunchActivated == false?
+            ToastContent content = new ToastContent()
+            {
+                Launch = "app-defined-string",
+
+                Visual = new ToastVisual()
+                {
+                    BindingGeneric = new ToastBindingGeneric()
+                    {
+                        Children =
+                            {
+                                new AdaptiveText()
+                                {
+                                    Text = texto
+                                }
+                            }
+                    }
+                },
+
+                Actions = new ToastActionsCustom()
+                {
+                    Buttons =
+                                {
+                                    new ToastButton("Aceptar", "Aceptado")
+                                }
+                },
+                Audio = new ToastAudio()
+                {
+                    Src = new Uri("ms-winsoundevent:Notification.Reminder")
+                }
+            };
+            //Enviar la notificación
+            ToastNotification notificar = new ToastNotification(content.GetXml());
+            ToastNotifier aviso = ToastNotificationManager.CreateToastNotifier();
+            aviso.Show(notificar);
+        }
+
+        /// <summary>
+        /// Se invoca cuando la aplicación la inicia normalmente el usuario final. Se usarán otros puntos
+        /// </summary>
+        /// <param name="sender">Marco que produjo el error de navegación</param>
+        /// <param name="e">Detalles sobre el error de navegación</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// Se invoca al suspender la ejecución de la aplicación. El estado de la aplicación se guarda
+        /// sin saber si la aplicación se terminará o se reanudará con el contenido
+        /// de la memoria aún intacto.
+        /// </summary>
+        /// <param name="sender">Origen de la solicitud de suspensión.</param>
+        /// <param name="e">Detalles sobre la solicitud de suspensión.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            NotificarLifeCicle("Aplicación Suspendida");
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Guardar el estado de la aplicación y detener toda actividad en segundo plano
+            deferral.Complete();
+        }
+    }
+}
